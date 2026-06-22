30cd9f87b59e004f25c48afa1807aa4ad8dca1e9
mitchell-baye <Mitchell.Baye@colorado.edu>
2026-06-21 20:41:15 -0600

    Assignment 9 commit 4

--- base_external/package/aesd-socketApp/aesd-assignments.mk
+++ base_external/package/aesd-socketApp/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_SOCKETAPP_VERSOIN = bbf0f865a9438a4b15ab0fb3449fde50917dbd47
+AESD_SOCKETAPP_VERSION = f955072c63ea23a27a1e3bf5b8805d3f62e70792
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,7 +15,7 @@ AESD_SOCKETAPP_SITE_METHOD = git
 AESD_SOCKETAPP_GIT_SUBMODULES = YES
 AESD_SOCKETAPP_MAKE_OPTS += USE_AESD_CHAR_DEVICE=1
 define AESD_SOCKETAPP_BUILD_CMDS
-	$(MAKE) CC=$(TARGET_CC) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+	$(MAKE) CC=$(TARGET_CC) $(TARGET_CONFIGURE_OPTS) $(AESD_SOCKETAPP_MAKE_OPTS) -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
